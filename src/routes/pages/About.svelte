fb532d3824529281d4c9ee0342c0fdbb1603ff8c
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-06-30 19:36:29 +0200

    Update About.svelte

--- src/routes/pages/About.svelte
+++ src/routes/pages/About.svelte
@@ -1,12 +1,9 @@
 <script>
-	import Card from '../components/Card.svelte'
-	
 	import price from '$lib/assets/PJSK Reroll 2024.png'
-
 </script>
 
 <div class="text-white flex flex-col justify-center">
-	<img src={price} alt='price list' class="w-full h-full"/>
+	<img src={price} alt='PJSK Reroll' class="w-full h-full"/>
 </div>
 
 
