298d09018261485a4a7291c063567003b650ad69
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-09-23 12:34:49 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,24 +9,13 @@
 			<div class="text-center">
 				
 				<p class="text-white font-light text-xl sm:text-2xl mb-8 leading-relaxed">
-					Hiện tại mình đang chạy thử web bán acc tự động, mọi người có thể ghé qua để coi acc cũng như mua acc ở link dưới nha:
+					Mọi người có thể ghé qua để coi acc cũng như mua acc ở link dưới nha:
 				</p>
 
 				<a href="https://game.bellasekai.com/" target="_blank" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-8 rounded-3xl text-xl sm:text-2xl mb-8 transition duration-300 ease-in-out">
 					Truy cập Bella Sekai
 				</a>
 
-				<p class="text-white font-light text-xl sm:text-2xl mb-8 leading-relaxed">
-					Nếu đã ghé qua, hi vọng mọi người có thể điền form khảo sát để mình có thể hoàn thiện web nha:
-				</p>
-
-				<a href="https://docs.google.com/forms/d/e/1FAIpQLScnFEAcPUs29SCY7oEKdgjU8aPAaVwrzXsQBdZSxSTt3lUYew/viewform" target="_blank" class="mb-8 inline-block bg-green-500 hover:bg-green-600 text-white font-bold py-4 px-8 rounded-3xl text-xl sm:text-2xl transition duration-300 ease-in-out">
-					Điền Khảo sát Trải nghiệm Người dùng
-				</a>
-
-				<p class="text-white font-light text-xl sm:text-2xl leading-relaxed">
-					Mình cám ơn mọi người rất nhiều!
-				</p>
 			</div>
 		</div>
 	</div>
